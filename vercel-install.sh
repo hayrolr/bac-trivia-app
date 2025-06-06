5744acfbf6a8c82ec106ccf914b12fd353114518
Hayrol Reyes <hayrolr@gmail.com>
2025-06-06 13:24:13 -0600

    20250606-1300: MindAR version update
    - Add install script to override Vercel default and solve the MindAR library installation problems

--- vercel-install.sh
+++ vercel-install.sh
@@ -9,11 +9,9 @@ echo "Vercel Install Script: Started"
 # The '-y' flag automatically answers 'yes' to any installation prompts.
 echo "Vercel Install Script: Installing system dependencies for node-canvas..."
 apt-get update && apt-get install -y build-essential libcairo2-dev libpango1.0-dev libjpeg-dev libgif-dev librsvg2-dev
-
 echo "Vercel Install Script: System dependencies installed successfully."
 
 # 2. Proceed with the standard npm installation
 echo "Vercel Install Script: Running 'npm install'..."
 npm install
-
 echo "Vercel Install Script: Finished successfully."
